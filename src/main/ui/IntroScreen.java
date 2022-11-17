bc36f723b6c8a66db672a59f4defd7937e4af02b
Parsa <parsaha@students.cs.ubc.cs>
2022-11-17 10:58:58 -0800

    Office hour

---
+++ src/main/ui/IntroScreen.java
@@ -0,0 +1,10 @@
+package ui;
+
+import javax.swing.*;
+
+public class IntroScreen extends JFrame {
+
+    public IntroScreen() {
+        super("VimPad Splash");
+    }
+}
--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,8 +1,5 @@
 package ui;
 
-import javax.swing.*;
-import java.io.FileNotFoundException;
-
 // Attribution[1]: The try-catch was modified with respect to the main function in "JsonSerializationDemo"
 public class Main {
     public static void main(String[] args) {
